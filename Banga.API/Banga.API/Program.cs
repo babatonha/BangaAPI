086580dafaad1694a0f8ea9dd1e51047c3d3e6ab
babatonha <tonhaview@gmail.com>
2024-10-25 20:11:27 +0200

    updated code.

--- Banga.API/Banga.API/Program.cs
+++ Banga.API/Banga.API/Program.cs
@@ -2,6 +2,7 @@ using Banga.Data;
 using Banga.Logic.Extensions;
 using Banga.Logic.Middleware;
 using Banga.Logic.SignalR;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddDbContext<DatabaseContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -23,21 +25,25 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//app.UseStaticFiles();
+app.UseRouting();
 
 app.UseCors(corsPolicyBuilder => corsPolicyBuilder
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200", "http://localhost:1920"));
+    .WithOrigins("https://easyestate.online", "http://easyestate.online", "http://localhost:4200", "http://localhost:1920"));
+
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -46,20 +52,21 @@ app.UseAuthorization();
 app.UseHttpsRedirection();
 
 app.MapControllers();
-app.MapHub<PresenceHub>("hubs/presence");
-app.MapHub<MessageHub>("hubs/message");
+//app.MapHub<PresenceHub>("hubs/presence");
+//app.MapHub<MessageHub>("hubs/message");
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DatabaseContext>();
-    await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [Connections]");
-}
-catch (Exception)
-{
+//using var scope = app.Services.CreateScope();
+//var services = scope.ServiceProvider;
+//try
+//{
+//    var context = services.GetRequiredService<DatabaseContext>();
+//    await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [Connections]");
+//}
+//catch (Exception)
+//{
 
-    throw;
-}
+//    throw;
+//}
 
 app.Run();
+
